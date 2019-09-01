3dcba5b46661377393154bfb5567892d57e7a59e
InkyChew <B10623019@yuntech.edu.tw>
2019-09-01 14:54:05 +0800

    Updates

---
+++ precache-manifest.3ce1fa2a21f3da21de97be2ed6e16b9f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "c64c95dd95648a5f27b44f73af20d5d8",
+    "url": "img/logo-rapid-chat.c64c95dd.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "f70714e0cb7a5a1cc88e60c36d040232",
+    "url": "img/sticker-thx.f70714e0.svg"
+  },
+  {
+    "revision": "95ba029fa607d2d929af",
+    "url": "css/app.300c385e.css"
+  },
+  {
+    "revision": "4990f62ecc11c87877f8",
+    "url": "js/chunk-vendors.bd5855f5.js"
+  },
+  {
+    "revision": "0b8f9cca04cc46401c903c03664c62f0",
+    "url": "img/sticker-no.0b8f9cca.svg"
+  },
+  {
+    "revision": "50f3ffb64c0e9832650146a99c0bffd6",
+    "url": "img/bg-pattern-unit.50f3ffb6.svg"
+  },
+  {
+    "revision": "2c75753cc851fae2ac5bfc380c5e07e7",
+    "url": "img/thumbnail-1.2c75753c.svg"
+  },
+  {
+    "revision": "858d0e15554bb0e7db02419abbf6806d",
+    "url": "img/thumbnail-2.858d0e15.svg"
+  },
+  {
+    "revision": "4083184b1632914ea1ab2ee99184fa35",
+    "url": "img/thumbnail-3.4083184b.svg"
+  },
+  {
+    "revision": "bd6c3fd433e0bf0a857c",
+    "url": "js/about.d6d3d59f.js"
+  },
+  {
+    "revision": "95ba029fa607d2d929af",
+    "url": "js/app.11ad689a.js"
+  },
+  {
+    "revision": "baa5687e05d4585116eee4b72da40cc0",
+    "url": "img/thumbnail-4.baa5687e.svg"
+  },
+  {
+    "revision": "cda3a13aecb3002d8f3e4dfb4898376e",
+    "url": "img/sticker-wtf.cda3a13a.svg"
+  },
+  {
+    "revision": "aa8c358bfc1db7f6350487097e0d09c6",
+    "url": "img/sticker-hi.aa8c358b.svg"
+  },
+  {
+    "revision": "eab7458ac1c2da3f9a82eaf63febe075",
+    "url": "img/thumbnail-6.eab7458a.svg"
+  },
+  {
+    "revision": "98a69daf420025feb473a1ac0e9983ed",
+    "url": "img/sticker-lol.98a69daf.svg"
+  },
+  {
+    "revision": "1556a196c43863ed39247ec6d31ef7e5",
+    "url": "img/thumbnail-5.1556a196.svg"
+  },
+  {
+    "revision": "44bf026a22754e4123fd475337aef054",
+    "url": "img/sticker-ok.44bf026a.svg"
+  },
+  {
+    "revision": "31f259199d1c3425203e7840c2fdb9f5",
+    "url": "index.html"
+  },
+  {
+    "revision": "bd6c3fd433e0bf0a857c",
+    "url": "css/about.93f22f6c.css"
+  }
+];
